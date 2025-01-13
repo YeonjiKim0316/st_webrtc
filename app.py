fd9fbb197e72dc41b369cde33955f4f431526c99
YeonjiKim0316 <73862452+YeonjiKim0316@users.noreply.github.com>
2025-01-13 17:27:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,19 +16,22 @@ def video_frame_callback(frame):
     results = model(img)
 
     # 감지된 객체에 대한 경계 상자와 레이블 그리기
-    for result in results:
-        boxes = result.boxes
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            
-            # 클래스 이름과 신뢰도 표시
-            cls = int(box.cls[0])
-            conf = float(box.conf[0])
-            label = f"{model.names[cls]} {conf:.2f}"
-            cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
+    if results:
+        for result in results:
+            boxes = result.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                
+                # 클래스 이름과 신뢰도 표시
+                cls = int(box.cls[0])
+                conf = float(box.conf[0])
+                label = f"{model.names[cls]} {conf:.2f}"
+                cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    else:
+        break
+        
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 webrtc_streamer(
