5092a6afb6e75dea56f01f22f6c0f7cfc78f3098
YeonjiKim0316 <73862452+YeonjiKim0316@users.noreply.github.com>
2025-01-13 17:12:44 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import cv2
+import av
+
+st.title("간단한 Streamlit WebRTC 앱")
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+webrtc_streamer(
+    key="example",
+    video_frame_callback=video_frame_callback,
+    rtc_configuration={
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    }
+)
