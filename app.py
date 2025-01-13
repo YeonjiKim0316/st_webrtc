f77ec622c6544e926a7ee1df1532e779685b68ff
YeonjiKim0316 <73862452+YeonjiKim0316@users.noreply.github.com>
2025-01-13 17:35:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -46,8 +46,7 @@ webrtc_streamer(
         "iceServers": [
             {"urls": ["stun:stun.l.google.com:19302"]},
             {"urls": ["stun:stun1.l.google.com:19302"]},
-            {"urls": ["stun:stun2.l.google.com:19302"]},
-            {"urls": ["stun:global.stun.twilio.com:3478?transport=udp"]}
+            {"urls": ["stun:stun2.l.google.com:19302"]}
         ]
     },
     media_stream_constraints={
